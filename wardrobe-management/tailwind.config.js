e71386934b5b816bcec8fe2fcfab9e5a68048c94
vincent munywoki <vincentmunywoki12@gmail.com>
2025-02-26 11:30:55 +0300

    tailwind.css

--- wardrobe-management/tailwind.config.js
+++ wardrobe-management/tailwind.config.js
@@ -1,1 +1,6 @@
-export default { content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], theme: { extend: {} }, plugins: [], };
+export default { 
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'], 
+    theme: { 
+        extend: {} }, 
+    plugins: [], 
+};
