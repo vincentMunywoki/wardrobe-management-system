850a3220b5d2e8438520c969bbba2d05780b0004
vincent munywoki <vincentmunywoki12@gmail.com>
2025-02-27 00:41:13 +0300

    created AuthController.php

--- wardrobe-backend/app/Http/Controllers/AuthController.php
+++ wardrobe-backend/app/Http/Controllers/AuthController.php
@@ -3,8 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully!'], 201);
+    }
+
+    public function login(Request $request) {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'Invalid login credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
 }
