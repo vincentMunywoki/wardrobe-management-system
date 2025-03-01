65b54e931a1d877ee61056b95918581219c51164
vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2025-03-02 01:02:21 +0300

    Update Kernel.php

--- wardrobe-backend/app/Http/Kernel.php
+++ wardrobe-backend/app/Http/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends HttpKernel
      *
      * @var array<int, class-string|string>
      */
+    
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         'api' => [
