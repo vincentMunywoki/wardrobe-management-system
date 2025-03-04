795cc854f7e87d44628f35eddec2b046162e1816
vincent munywoki <87576661+vincentMunywoki@users.noreply.github.com>
2025-03-04 14:15:20 +0300

    Update api.js

--- wardrobe-frontend/src/api.js
+++ wardrobe-frontend/src/api.js
@@ -6,6 +6,7 @@ const api = axios.create({
         "Content-Type": "application/json",
         Accept: "application/json",
     },
+    
     withCredentials: true, // Required for Laravel Sanctum
 });
 
